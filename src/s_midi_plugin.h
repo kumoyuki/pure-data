60eed8f14f05274733315f23ec4b8b62e176941e
Aditi DeAngelis <kumoyuki@gmail.com>
2024-04-17 02:54:04 +0100

    midi plugin documentation/commentary

--- src/s_midi_plugin.h
+++ src/s_midi_plugin.h
@@ -8,12 +8,23 @@ struct midi_plugin
 {
     char const* mp_name;
 
+        /* mp_init may be called multiple times during a PD run. it needs to
+         * be able to understand enough of its context to do the right thing\
+         * *AND* not clobber any user configuration done since the last
+         * invocation
+         */
     void (*mp_init)();
+
     void (*mp_open_midi)(int nmidiin, int *midiinvec, int nmidiout, int *midioutvec);
     void (*mp_close_midi)(void);
     void (*mp_putmidimess)(int portno, int a, int b, int c);
     void (*mp_putmidibyte)(int portno, int byte);
     void (*mp_poll_midi)(void);
+
+        /* note that in the normal order of things, mP-getdevs will be called *before* mp_init()
+         * this means that it may need to call mp_init for iteself, depending on the particular
+         * midi plugin.
+         */
     void (*mp_getdevs) (
         char *indevlist, int *nindevs,
         char *outdevlist, int *noutdevs, int maxndev, int devdescsize);
